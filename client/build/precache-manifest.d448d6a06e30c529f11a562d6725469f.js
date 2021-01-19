6377d537888dbd902965352ae30531d07edbd665
galacticnative <68270151+galacticnative@users.noreply.github.com>
2021-01-18 20:35:08 -0700

    build

---
+++ client/build/precache-manifest.d448d6a06e30c529f11a562d6725469f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018bc045bc87b4e9efb31a3891433111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "841095bb52fdfad48979",
+    "url": "/static/css/main.e97233f6.chunk.css"
+  },
+  {
+    "revision": "88ef8da157a27dcc00f8",
+    "url": "/static/js/2.c9b2c329.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.c9b2c329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "841095bb52fdfad48979",
+    "url": "/static/js/main.e0dfbd72.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
