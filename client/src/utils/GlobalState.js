65406abba1d0a90c211c808f58a992a562a46a0a
galacticnative <68270151+galacticnative@users.noreply.github.com>
2021-01-18 17:00:55 -0700

    commit

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -5,13 +5,13 @@ const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-        products: [],
-        cart: [],
-        cartOpen: false,
-        categories: [],
-        currentCategory: ''
-    });
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    cart: [],
+    cartOpen: false,
+    categories: [],
+    currentCategory: '',
+  });
 
   return <Provider value={[state, dispatch]} {...props} />;
 };
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,6 +8,7 @@ const resolvers = {
     categories: async () => {
       return await Category.find();
     },
+
     products: async (parent, { category, name }) => {
       const params = {};
 
@@ -20,12 +21,13 @@ const resolvers = {
           $regex: name
         };
       }
-
       return await Product.find(params).populate('category');
     },
+
     product: async (parent, { _id }) => {
       return await Product.findById(_id).populate('category');
     },
+
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
@@ -37,9 +39,9 @@ const resolvers = {
 
         return user;
       }
-
       throw new AuthenticationError('Not logged in');
     },
+
     order: async (parent, { _id }, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
@@ -49,14 +51,13 @@ const resolvers = {
 
         return user.orders.id(_id);
       }
-
       throw new AuthenticationError('Not logged in');
     },
+
     checkout: async (parent, args, context) => {
       const url = new URL(context.headers.referer).origin;
       const order = new Order({ products: args.products });
       const { products } = await order.populate('products').execPopulate();
-
       const line_items = [];
 
       for (let i = 0; i < products.length; i++) {
@@ -66,32 +67,32 @@ const resolvers = {
           description: products[i].description,
           images: [`${url}/images/${products[i].image}`]
         });
-
-        const session = await stripe.checkout.sessions.create({
-          payment_method_types: ['card'],
-          line_items,
-          mode: 'payment',
-          success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-          cancel_url: `${url}/`
-        });
-        
-        return { session: session.id };
-
+      
         // generate price id using the product id
         const price = await stripe.prices.create({
           product: product.id,
           unit_amount: products[i].price * 100,
           currency: 'usd',
         });
-
+      
         // add price id to the line items array
         line_items.push({
           price: price.id,
           quantity: 1
         });
       }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
+      return { sessions: session.id };
     }
   },
+
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
